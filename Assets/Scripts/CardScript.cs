b066b38fbc34d2e80e3a4c951f985bfbbff64260
Rasmus <rasmuspuls@live.com>
2017-03-06 12:55:12 +0100

    card script added

--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class CardScript : MonoBehaviour {
 
+	//GameObject selectedCard;
+
 	// Use this for initialization
 	void Start () {
-		
+		//selectedCard = GameOGetComponent;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void FlipCard(){
+		print ("test");
+		gameObject.transform.Rotate(new Vector3(0,0,180));
 	}
 }
