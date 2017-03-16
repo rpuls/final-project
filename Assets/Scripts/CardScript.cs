83aa8b32a688dbb431df7928fa7b64a449d7cb89
Rasmus <rasmuspuls@live.com>
2017-03-16 12:16:45 +0100

    animation updated, cards are now moved to screen width/2, height/2 instead of fixed coordinates. OBS: "pickCard()" is not called from the trigger for some reason! but the method is working

--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -7,30 +7,54 @@ public class CardScript : MonoBehaviour {
 	public List<GameObject> attackCards;
 	public static float animationSpeed = 2f;
 
-	// Use this for initialization
+	//private static Vector3 centerPos;
+	private static int middleX = Screen.width / 2;
+	private static int middleY = Screen.height / 2;
+
+	//hashtables for itween
+
+	//flip
+	Hashtable flip1 = iTween.Hash("y", 90, "time", animationSpeed,"easetype", iTween.EaseType.easeInQuad);
+	Hashtable flip2 = iTween.Hash("y", 180, "time", animationSpeed, "easetype", iTween.EaseType.easeOutQuart);
+	Hashtable moveToCenter = iTween.Hash("x", middleX, "y", middleY, "time", animationSpeed);
+	Hashtable scaleX2 = iTween.Hash("x", 2, "y", 2, "time", animationSpeed);
+	//pick
+	Hashtable scaleX05 = iTween.Hash("x", 0.5f, "y", 0.5f, "time", animationSpeed);
+	Hashtable moveToConor = iTween.Hash("x", 100, "y", 100, "time", animationSpeed);
+	//shake
+	Hashtable shake = iTween.Hash("z", 5, "speed", 1f);
+
+
+
 	void Start () {
-		
+
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		
 	}
 
-
+	//public methods to call from trigger.
 	public void FlipCard(){
 		GameObject newĆard = attackCards [Random.Range( 0, attackCards.Count )];
 		StartCoroutine (TurnCard(newĆard));
 		newĆard.transform.Rotate (0, 90, 0);
-
 	}
 
-	Hashtable flip1 = iTween.Hash("y", 90, "time", animationSpeed,"easetype", iTween.EaseType.easeInQuad);
-	Hashtable flip2 = iTween.Hash("y", 180, "time", animationSpeed, "easetype", iTween.EaseType.easeOutQuart);
-	Hashtable moveToCenter = iTween.Hash("x", 730, "y", 350, "time", animationSpeed);
-	Hashtable scale = iTween.Hash("x", 2, "y", 2, "time", animationSpeed);
+	public void PickCard(){
+		iTween.MoveTo (gameObject, moveToConor);
+		iTween.ScaleTo (gameObject, scaleX05);
+		Debug.Log ("xxx");
+		print ("yyy");
+	}
+
+	public void ShakeCard(){
+		iTween.ShakeRotation (gameObject, shake);
+	}
 
 
+
+	//coroutines and other logic.
 	public IEnumerator TurnCard(GameObject otherCard){
 		iTween.RotateTo (gameObject, flip1);
 		yield return new WaitForSeconds (animationSpeed);
@@ -38,8 +62,8 @@ public class CardScript : MonoBehaviour {
 		otherCard.SetActive (true);
 		iTween.RotateTo (otherCard, flip2);
 		iTween.MoveTo (otherCard, moveToCenter);
-		iTween.ScaleTo (otherCard, scale);
+		iTween.ScaleTo (otherCard, scaleX2);
 		yield return new WaitForSeconds (animationSpeed);
 	}
-
+		
 }
