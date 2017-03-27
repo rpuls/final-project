75d72d82fa8b84e5b3b4f2aee07b7b10bd2de1cd
Daniel <danielhollmann@hotmail.com>
2017-03-27 16:08:56 +0200

    The Player Stats are now set in menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,34 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
 
     public static bool hasChosenCard;
-	public enum ActivePlayer { P1, P2 };
 	public List<GameObject> playerPrefabs;
+	public PlayerStats playerOne;
+	public PlayerStats playerTwo;
 
-	void Start() { 
-        
-    }
-		
-    
-    void Update() {
-
-
-    }
-
-	public void setPlayerCalss(){
-
+	public void setPlayerOne(){
+		setUpPlayerclass(playerOne);
+		setUpPlayerUserName(playerOne);
 	}
 
-	public void createPlayer(){
-		GameObject.Find ("Name Input").GetComponent< find type her> ();
-		GameObject.Find ("Label").GetComponent<TextMesh> ();
-		//instanciate
-
+	private void setUpPlayerclass(PlayerStats ps){
+		GameObject classDropDown = GameObject.FindWithTag("PlayerOneClass");
+		int menuIndex = classDropDown.GetComponent<Dropdown> ().value;
+		List<Dropdown.OptionData> menuOptions = classDropDown.GetComponent<Dropdown> ().options;
+		string selected = (menuOptions [menuIndex].text);
+		ps.gameClass = selected;
 	}
+
+	private void setUpPlayerUserName(PlayerStats ps){
+		GameObject nameInputField = GameObject.FindWithTag("PlayerUserName");
+		string username = nameInputField.GetComponent<InputField>().text;
+		Debug.Log(username);
+		ps.userName = username;
+	}
+
 }
 
 
