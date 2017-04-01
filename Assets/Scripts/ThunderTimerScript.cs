847bb9c869800e1a83b103c2b98a04d59252ef31
Rasmus <rasmuspuls@live.com>
2017-04-02 00:10:16 +0200

    graphics and sound fx added. ´thunder script updated

--- Assets/Scripts/ThunderTimerScript.cs
+++ Assets/Scripts/ThunderTimerScript.cs
@@ -5,43 +5,48 @@ using UnityEngine.UI;
 
 public class ThunderTimerScript : MonoBehaviour {
 
-	private float m_Position = 0f;					// The starting position in % of where the thundercloud will spawn
 	public Slider m_Slider;							// The slider to represent where the cloud is
 	public Image m_FillImage;						// The image component of the slider.
-	public Color m_FullHealthColor = Color.green;	// The color the health bar will be when on full health.
-	public Color m_ZeroHealthColor = Color.red;		// The color the health bar will be when on no health.
 	public GameObject m_ThunderStroke;				// A "thunder" prefab that will be instantiated when space i tabbed, and spawns where the cloud is
-
+	public AudioSource m_ThunderStrikeSound;
+	private bool move = true;						// Used to toggle movement
+	private bool isMovingFoward = true;				// Used to change direction forth/back
+	private float position = 0f;					// The starting position in % of where the thundercloud will spawn
+	private float tempTime;							// Used for delay
 	// Use this for initialization
 	void Start (){
 		
 	}
 	
 	// Update is called once per frame
-	float tempTime;
+
 	void Update (){
-		tempTime += Time.deltaTime;
-		if (tempTime > 0.01) {
-			tempTime = 0;
+		if (move) {
 			moveCloud ();
+		}
+
+		if (Input.GetKeyDown ("space")) {
+			move = false;
 		}
 	}
 
-	bool isMovingFoward = true;
-	public void moveCloud (){
-		
 
-		if (isMovingFoward) {
-			m_Position += 0.03f;
-			if (m_Position >= 0.99f) {
-				isMovingFoward = false;
+	public void moveCloud (){
+		tempTime += Time.deltaTime;
+		if (tempTime > 0.01) {
+			tempTime = 0;
+			if (isMovingFoward) {
+				position += 0.03f;
+				if (position >= 0.99f) {
+					isMovingFoward = false;
+				}
+			} else {
+				position -= 0.03f;
+				if (position <= 0.01f) {
+					isMovingFoward = true;
+				}
 			}
-		} else {
-			m_Position -= 0.03f;
-			if (m_Position <= 0.01f) {
-				isMovingFoward = true;
-			}
+			m_Slider.value = position;
 		}
-		m_Slider.value = m_Position;
 	}
 }
