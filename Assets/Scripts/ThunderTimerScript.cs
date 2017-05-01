a5d2cc075f0f2c3fde0e31665842893bb57544cb
Rasmus <rasmuspuls@live.com>
2017-05-01 13:17:36 +0200

    thunder script & animation prefab updated

--- Assets/Scripts/ThunderTimerScript.cs
+++ Assets/Scripts/ThunderTimerScript.cs
@@ -8,10 +8,12 @@ public class ThunderTimerScript : MonoBehaviour {
 	public Slider m_Slider;							// The slider to represent where the cloud is
 	public Image m_FillImage;						// The image component of the slider.
 	public GameObject m_ThunderStroke;				// A "thunder" prefab that will be instantiated when space i tabbed, and spawns where the cloud is
-	public AudioSource m_ThunderStrikeSound;
+	public AudioSource m_ThunderStrikeSound;		// Sound source
+	public GameObject m_thunderAnimation;			// animation prefab source
 	private bool move = true;						// Used to toggle movement
 	private bool isMovingFoward = true;				// Used to change direction forth/back
 	private float position = 0f;					// The starting position in % of where the thundercloud will spawn
+	public float thunderDifficulty = 0.9f;			// low = easy, max difficulty = 0.99f
 	private float tempTime;							// Used for delay
 	// Use this for initialization
 	void Start (){
@@ -28,8 +30,18 @@ public class ThunderTimerScript : MonoBehaviour {
 	private void Shoot(){
 		if (Input.GetKeyDown ("space")) {
 			move = false;
-			m_ThunderStrikeSound.Play ();
+			if (position > thunderDifficulty) {
+				m_ThunderStrikeSound.Play ();
+				m_thunderAnimation.SetActive (true);
+				StartCoroutine(StrikeAnimator (m_thunderAnimation));
+			}
 		}
+	}
+
+	private IEnumerator StrikeAnimator(GameObject thunder_prefab){
+		thunder_prefab.SetActive (true);
+		yield return new WaitForSeconds (5);
+		thunder_prefab.SetActive (false);
 	}
 
 	public void MoveCloud (){
