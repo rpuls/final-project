49485b56a661e2f98917d230100d4f527a915dd1
Rasmus <rasmuspuls@live.com>
2017-05-29 17:09:45 +0200

    both walls now blocks seasnake and katana now

--- Assets/Scripts/MoveScripts/KatanaScript.cs
+++ Assets/Scripts/MoveScripts/KatanaScript.cs
@@ -6,10 +6,10 @@ public class KatanaScript : MonoBehaviour, IMove {
 
 	public GameObject KatanaPrefab;
 	public TurnManager turnmanager;
+	public GameObject opponentWall;
 	private Vector3 StartPos = new Vector3(-124f,16f,0f);
 	private Hashtable Attack = iTween.Hash("x", 110f, "y", 16f, "z", 0f, "time", 1);//, "easetype", iTween.EaseType.easeInQuad);
 	private Hashtable AttackFail = iTween.Hash("x", 63f, "y", 16f, "z", 0f, "time", 1);//, "easetype", iTween.EaseType.easeInQuad);
-	private bool OponentHaveWall;
 
 	// Use this for initialization
 	void Start () {
@@ -35,7 +35,7 @@ public class KatanaScript : MonoBehaviour, IMove {
 		yield return new WaitForSeconds (0.5f);
 		KatanaPrefab.SetActive (true);
 		yield return new WaitForSeconds (1f);
-		if (OponentHaveWall) {
+		if (opponentWall.activeInHierarchy) {
 			iTween.MoveTo (KatanaPrefab, AttackFail);
 			yield return new WaitForSeconds (1.1f);
 		} else {
@@ -51,6 +51,5 @@ public class KatanaScript : MonoBehaviour, IMove {
 	public void CleanUp(){
 		KatanaPrefab.transform.position = StartPos;
 		KatanaPrefab.SetActive (false);
-		OponentHaveWall = false;
 	}
 }
--- Assets/Scripts/MoveScripts/SeaDragonScript.cs
+++ Assets/Scripts/MoveScripts/SeaDragonScript.cs
@@ -6,11 +6,11 @@ public class SeaDragonScript : MonoBehaviour, IMove {
 
 	public GameObject SeaSnakePrefab;
 	public TurnManager turnmanager;
+	public GameObject opponentWall;
 	private Vector3 StartPos = new Vector3(145,-18f,6.5f);
 	private Hashtable Engange = iTween.Hash("x", 145f, "y", -1f, "z", 6.5f, "time", 1);//, "easetype", iTween.EaseType.easeInQuad);
 	private Hashtable Attack = iTween.Hash("x", -97f, "y", 7f, "z", 6.5f, "time", 1);//, "easetype", iTween.EaseType.easeInQuad);
 	private Hashtable AttackFail = iTween.Hash("x", -45f, "y", 7f, "z", 6.5f, "time", 1);//, "easetype", iTween.EaseType.easeInQuad);
-	private bool OponentHaveWall;
 
 	// Use this for initialization
 	void Start () {
@@ -25,7 +25,7 @@ public class SeaDragonScript : MonoBehaviour, IMove {
 	public void DoMove()
 	{
 		CleanUp();
-		//some how, check if oponen have wall defense active and set OponentHaveWall = true
+
 		this.gameObject.SetActive(true);
 		StartCoroutine (SeaAttack ());
 	}
@@ -37,7 +37,7 @@ public class SeaDragonScript : MonoBehaviour, IMove {
 		yield return new WaitForSeconds (2f);
 		iTween.MoveTo (SeaSnakePrefab, Engange);
 		yield return new WaitForSeconds (1.1f);
-		if (OponentHaveWall) {
+		if (opponentWall.activeInHierarchy) {
 			iTween.MoveTo (SeaSnakePrefab, AttackFail);
 			yield return new WaitForSeconds (1.1f);
 		} else {
@@ -52,6 +52,5 @@ public class SeaDragonScript : MonoBehaviour, IMove {
 	public void CleanUp(){
 		SeaSnakePrefab.transform.position = StartPos;
 		SeaSnakePrefab.transform.rotation = Quaternion.Euler(-90, 0, -90);
-		OponentHaveWall = false;
 	}
 }
